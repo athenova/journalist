2c17417128d3820a8046fc7d5e8069ae235504d0
athenova <aleksey.sergeyev@yandex.com>
2025-06-24 19:21:41 +0300

    Рандом плохо работает

--- projects/no_one_before.py
+++ projects/no_one_before.py
@@ -1,25 +1,22 @@
-from simple_blogger.poster.telegram import TelegramPoster
-from simple_blogger.poster.instagram import InstagramPoster
-from simple_blogger.poster.vk import VkPoster
+import json
 from simple_blogger.blogger import Journalist
+from simple_blogger.poster.telegram import TelegramPoster
+from simple_blogger.poster.vk import VkPoster
+from simple_blogger.poster.instagram import InstagramPoster
 from simple_blogger.preprocessor.text import TagAdder
-from simple_blogger.generator.openai import OpenAiTextGenerator
-import datetime
+from datetime import date, timedelta
 
-class HistoricBlogger(Journalist):
-    # def _system_prompt(self):
-    #     return self.system_prompt
-
-    def _message_generator(self):
-        return OpenAiTextGenerator(self.system_prompt)
+class HistorianBlogger(Journalist):
+    
+    def _system_prompt(self):
+        return "Ты - историк. Я дам тебе одну из пар: спортсмен-рекорд, изобретатель-изобретение, физик-открытие, химик-открытие, путешественник-открытие, философ-открытие, математик-открытие. Расскажи инетересный факт, связанный с этой парой, используй смайлики и не более 150 слов. Не используй 'Окей' и 'Конечно'"
     
     def _prompt_constructor(self, _):
-        return self.prompt_constructor
+        return f"{self.task['author']} - {self.task['invention']}"
 
-    def __init__(self, system_prompt, prompt_constructor, tags):
-        self.system_prompt = system_prompt
-        self.prompt_constructor = prompt_constructor
-        tagadder = TagAdder(tags)
+    def __init__(self, task):
+        self.task = task
+        tagadder = TagAdder(['#первые', '#лучшие', '#ДоЭтогоНикто'])
         posters = [
             TelegramPoster(chat_id='@no_one_before', processor=tagadder),
             VkPoster(group_id='229838140', processor=tagadder),
@@ -28,50 +25,13 @@ class HistoricBlogger(Journalist):
         ]
         super().__init__(posters)
 
-def post(day=None):
-    match day or datetime.date.today().weekday():
-        case 0:
-            blogger = HistoricBlogger(
-                "Ты - историк спорта", 
-                "Выбери рандомный вид спорта. Расскажи про один спортивный рекорд этого вида спорта и о том, кто его поставил. Используй смайлики и не более 150 слов. Не используй 'Окей' и 'Конечно'",
-                ['#рекорды', '#первые', '#спорт', '#лучшие']
-            )
-        case 1:
-            blogger = HistoricBlogger(
-                "Ты - историк изобретений", 
-                "Расскажи про одно рандомное техническое изобретение и о том, кто его изобрёл. Используй смайлики и не более 150 слов. Не используй 'Окей' и 'Конечно'",
-                ['#изобретения', '#первые', '#творчество', '#лучшие']
-            )
-        case 2:
-            blogger = HistoricBlogger(
-                "Ты - научный историк", 
-                "Расскажи про одно рандомное физическое открытие и о том, кто его открыл. Используй смайлики и не более 150 слов. Не используй 'Окей' и 'Конечно'",
-                ['#открытия', '#первые', '#наука', '#лучшие']
-            )
-        case 3:
-            blogger = HistoricBlogger(
-                "Ты - географический историк", 
-                "Расскажи про одно рандомное географическое открытие и о том, кто его открыл. Используй смайлики и не более 150 слов. Не используй 'Окей' и 'Конечно'",
-                ['#открытия', '#первые', '#география', '#лучшие']
-            )
-        case 4:
-            blogger = HistoricBlogger(
-                "Ты - научный историк", 
-                "Расскажи про одно рандомное химическое открытие и о том, кто его открыл. Используй смайлики и не более 150 слов. Не используй 'Окей' и 'Конечно'",
-                ['#открытия', '#первые', '#наука', '#лучшие']
-            )
-        case 5:
-            blogger = HistoricBlogger(
-                "Ты - философ-историк", 
-                "Расскажи про одно рандомное философское открытие и о том, кто его открыл. Используй смайлики и не более 150 слов. Не используй 'Окей' и 'Конечно'",
-                ['#открытия', '#первые', '#философия', '#лучшие']
-            )
-        case 6:
-            blogger = HistoricBlogger(
-                "Ты - математик", 
-                "Расскажи про одно рандомное матиматическое открытие и о том, кто его открыл. Используй смайлики и не более 150 слов. Не используй 'Окей' и 'Конечно'",
-                ['#открытия', '#первые', '#математика', '#лучшие']
-            )
+
+def post(offset=0):
+    tasks = json.load(open("./files/no_one_before.json", "rt", encoding="UTF-8"))
+    start_date = date(2025, 6, 25)-timedelta(days=offset)
+    today = date.today()
+    index = ((today - start_date).days + offset) % len(tasks)
+    blogger = HistorianBlogger(tasks[index])
     blogger.post()
 
 if __name__ == "__main__":

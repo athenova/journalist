887f1c473edab9aafe2a174732ca1f6d3c6139a3
athenova <aleksey.sergeyev@yandex.com>
2025-07-12 21:05:42 +0300

    Улучшения по темам. Рандом плохо работает.

--- projects/fridge_marathon.py
+++ projects/fridge_marathon.py
@@ -4,7 +4,7 @@ from simple_blogger.blogger import Journalist
 from simple_blogger.preprocessor.text import TagAdder
 from simple_blogger.generator.openai import OpenAiTextGenerator, GptImageGenerator
 from simple_blogger.generator.deepseek import DeepSeekTextGenerator
-import datetime
+import datetime, json
 
 class FridgeMarathoner(Journalist):
     # def _system_prompt(self):
@@ -36,13 +36,17 @@ def post(day=None):
     start_day = datetime.date(2025, 6, 8)
     today = datetime.date.today()
     day_count = (today - start_day).days
+    index = day_count // 7
     system_prompt=f"Ты - начинающий бегун, блогер с 1000000 подписчиков. У тебя лишний вес, отличное чувство юмора и позитивный настрой по жизни. Это твой {day_count} день бега. Ты бегаешь, чтобы есть, и ешь, чтобы бегать" 
     word_count = 150
     match day or datetime.date.today().weekday():
         case 0:
+            tasks = json.load(open("./files/fridge_marathon1.json", "rt", encoding="UTF-8"))
+            index = index % len(tasks)
+            task = tasks[index]
             blogger = FridgeMarathoner(
                 system_prompt, 
-                f"Расскажи, как ты выглядишь на тренировках. Посмейся над собой, не комплексуй, расскажи почему очень круто быть тобой. Используй смайлики и не более {word_count} слов. Не используй 'Окей' и 'Конечно'",
+                f"Расскажи про свою экипировку - {task['equip']}. Посмейся над собой, не комплексуй, расскажи почему очень круто быть тобой. Используй смайлики и не более {word_count} слов. Не используй 'Окей' и 'Конечно'",
                 "Нарисуй человека по описанию",
                 ['#Марафон', '#Холодильник', '#СпортДляВсех', '#ТелоСлучайностьДухРешает']
             )
@@ -54,16 +58,22 @@ def post(day=None):
                 ['#Марафон', '#Холодильник', '#СпортДляВсех', '#ПобедаНаВесахДуши']
             )
         case 2:
+            tasks = json.load(open("./files/fridge_marathon3.json", "rt", encoding="UTF-8"))
+            index = index % len(tasks)
+            task = tasks[index]
             blogger = FridgeMarathoner(
                 system_prompt, 
-                f"Расскажи про профилактику рандомной травмы бегунов с лишним весом. Используй смайлики и не более {word_count} слов. Не используй 'Окей' и 'Конечно'",
+                f"Расскажи про профилактику травмы '{task['injury']}'. Используй смайлики и не более {word_count} слов. Не используй 'Окей' и 'Конечно'",
                 "Нарисуй рисунок, вдохновлённый профилактикой травмы из описания, не рисуй текст",
                 ['#Марафон', '#Холодильник', '#СпортДляВсех', '#БегБезСлез']
             )
         case 3:
+            tasks = json.load(open("./files/fridge_marathon4.json", "rt", encoding="UTF-8"))
+            index = index % len(tasks)
+            task = tasks[index]
             blogger = FridgeMarathoner(
                 system_prompt, 
-                f"Выбери актуальную рандомную в беговом сообществе тему. Посмейся над ней. Расскажи, почему она для тебя актуальна. Используй смайлики и не более {word_count} слов. Не используй 'Окей' и 'Конечно'",
+                f"Посмейся над темой '{task['theme']}'. Расскажи, почему она для тебя актуальна. Используй смайлики и не более {word_count} слов. Не используй 'Окей' и 'Конечно'",
                 "Нарисуй рисунок, вдохновлённый темой из описания, не рисуй текст",
                 ['#Марафон', '#Холодильник', '#СпортДляВсех', '#БегПротивСтереотипов']
             )
@@ -75,9 +85,12 @@ def post(day=None):
                 ['#Марафон', '#Холодильник', '#СпортДляВсех', '#НеВесАЖизнь']
             )
         case 5:
+            tasks = json.load(open("./files/fridge_marathon6.json", "rt", encoding="UTF-8"))
+            index = index % len(tasks)
+            task = tasks[index]
             blogger = FridgeMarathoner(
                 system_prompt, 
-                f"Сегодня чит-мил день. Выбери рандомное блюдо с высоким содержанием сахара или углеводов. Расскажи его историю и о том, как ты, смакуя, съел его после тренировки. Используй смайлики и не более {word_count} слов. Не используй 'Окей' и 'Конечно'",
+                f"Сегодня день чит-мил. Расскажи историю блюда '{task['food']}' и о том, как ты, смакуя, съел его после тренировки. Используй смайлики и не более {word_count} слов. Не используй 'Окей' и 'Конечно'",
                 "Нарисуй блюдо из описания, не рисуй текст",
                 ['#Марафон', '#Холодильник', '#СпортДляВсех', '#НабегалНаСъел']
             )

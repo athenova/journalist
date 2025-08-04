455c922702721bae617a7ae9ef2794e7b78342aa
athenova <aleksey.sergeyev@yandex.com>
2025-08-04 18:21:56 +0300

    Переделал humanized

--- projects/humanized_the.py
+++ projects/humanized_the.py
@@ -4,23 +4,24 @@ from simple_blogger.poster.vk import VkPoster
 from simple_blogger.blogger import Journalist
 from simple_blogger.preprocessor.text import TagAdder
 from simple_blogger.generator.openai import OpenAiTextGenerator, OpenAiImageGenerator, GptImageGenerator
-import datetime
+import datetime, json
+from datetime import date
 
 class HumanizedBlogger(Journalist):
     def _message_generator(self):
         # return GptImageGenerator('Ты - художник с образованием психолога')
-        return OpenAiTextGenerator('Ты - художник с образованием психолога', api_key_name="GPT_API_KEY")
+        return OpenAiTextGenerator('Ты - художник с образованием психолога. Я дам тебе категорию и элемент категории. Опиши элемент, как если бы он был человеком', api_key_name="GPT_API_KEY")
     
     def _image_generator(self):
         return GptImageGenerator(api_key_name="GPT_API_KEY", system_prompt="Нарисуй, как бы выглядел человек по описанию")
         # return OpenAiImageGenerator(api_key_name="GPT_API_KEY", system_prompt="Нарисуй, как бы выглядел человек по описанию")
 
     def _prompt_constructor(self, _):
-        return f"Describe any random {self.subject}, if it were a human been. Дай ответ на русском языке, используй смайлики и не более 150 слов, не используй 'Конечно' и 'Okей'"
+        return f"{self.task['category']}, {self.task['subject']}"
 
-    def __init__(self, subject):
-        self.subject = subject
-        tagadder = TagAdder(['#иллюстрации', '#фантазии'])
+    def __init__(self, task):
+        self.task = task
+        tagadder = TagAdder(['#иллюстрации', '#фантазии', f"#{task['category']}"])
         posters = [
             TelegramPoster(chat_id='@humanized_the', processor=tagadder),
             VkPoster(group_id='229862079', processor=tagadder),
@@ -30,44 +31,11 @@ class HumanizedBlogger(Journalist):
         super().__init__(posters)
 
 def post(offset=0):
-    start_date = datetime.date(2025, 3, 26)
-    today = datetime.date.today()
-    subjects = [
-        "animal",
-        "book",
-        "browser",
-        "car brand",
-        "capital city",
-        "computer",
-        "game console",
-        "cosmic ship",
-        "country",
-        "cpu",
-        "delivery company",
-        "russian delivery company",
-        "car model",
-        "fashion brand",
-        "flower",
-        "fruit",
-        "game",
-        "famous house",
-        "insect",
-        "information technology brand",
-        "programming language",
-        "mobile phone",
-        "movie",
-        "photo camera",
-        "point of interest",
-        "river",
-        "ship",
-        "sneakers",
-        "song",
-        "stadium",
-        "tree",
-        "vegetable",
-    ]
-    index = ((today - start_date).days + offset) % len(subjects)
-    blogger = HumanizedBlogger(subjects[index])
+    tasks = json.load(open("./files/humanized_the.json", "rt", encoding="UTF-8"))
+    start_date = date(2025, 8, 5)
+    today = date.today()
+    index = ((today - start_date).days + offset) % len(tasks)
+    blogger = HumanizedBlogger(tasks[index])
     blogger.post()
 
 if __name__ == "__main__":

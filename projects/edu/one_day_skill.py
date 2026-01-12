7eccd43aaa298e624d8423cd658b80a92a7d1a70
athenova <aleksey.sergeyev@yandex.com>
2026-01-12 18:52:28 +0300

    Изменение структуры навыка

--- projects/edu/one_day_skill.py
+++ projects/edu/one_day_skill.py
@@ -13,19 +13,20 @@ class SkillBlogger(AutoSimpleBlogger):
         return f"./files/edu/one_day_skill.json"
     
     def _message_prompt_constructor(self, task):
-        return f"Расскажи как {task['name']}, используй не более 100 слов, используй смайлики"
+        return f"Расскажи как {task['name']}. Используй структуру: заглавие, основная часть. Используй смайлики и не более 100 слов."
     
     def _image_prompt_constructor(self, task):
         return f"Нарисуй рисунок, вдохновлённый темой '{task['name']}'"
         
-    def _message_generator(self):
-        return OpenAiTextGenerator(self._system_prompt())
+    # def _message_generator(self):
+    #     return OpenAiTextGenerator(self._system_prompt())
     
     def _image_generator(self):
         return OpenAiImageGenerator()
     
     def _posters(self):
         return [
+            # TelegramPoster(processor=tagadder),
             TelegramPoster(chat_id='@one_day_skill', processor=tagadder),
             VkPoster(group_id='229822298', processor=tagadder),
             # InstagramPoster(account_token_name='ONE_DAY_SKILL_TOKEN', account_id='9799308313442110', processor=tagadder)
